48e3962d657ad0369c84b10ba77b7668b2aa558f
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2014-10-13 13:18:09 +0200

    Added preservesSuperviewLayoutMargins property.

--- RATreeView/RATreeView.h
+++ RATreeView/RATreeView.h
@@ -461,6 +461,7 @@ typedef enum RATreeViewRowAnimation {
 @property (nonatomic) CGFloat estimatedRowHeight NS_AVAILABLE_IOS(7_0);
 @property (nonatomic) UIEdgeInsets separatorInset NS_AVAILABLE_IOS(7_0);
 @property (nonatomic) UIEdgeInsets layoutMargins NS_AVAILABLE_IOS(8_0);
+@property (nonatomic) BOOL preservesSuperviewLayoutMargins NS_AVAILABLE_IOS(8_0);
 @property (strong, nonatomic) UIView *backgroundView;
 
 
