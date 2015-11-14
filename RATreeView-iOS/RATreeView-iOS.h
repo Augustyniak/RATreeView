dbbe468cbd6f24eac97a5b3534ea8ff5f56a1f8c
Rafal Augustyniak <rafal@macoscope.net>
2015-11-14 22:54:43 +0100

    Add Carthage support

---
+++ RATreeView-iOS/RATreeView-iOS.h
@@ -0,0 +1,19 @@
+//
+//  RATreeView-iOS.h
+//  RATreeView-iOS
+//
+//  Created by Rafal Augustyniak on 14/11/15.
+//  Copyright © 2015 Rafal Augustyniak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RATreeView-iOS.
+FOUNDATION_EXPORT double RATreeView_iOSVersionNumber;
+
+//! Project version string for RATreeView-iOS.
+FOUNDATION_EXPORT const unsigned char RATreeView_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RATreeView_iOS/PublicHeader.h>
+
+
