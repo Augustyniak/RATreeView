896b9d0f7621d4fc6f94438cbd61aae4dba479c0
Rafał Augustyniak <rafalaugustyniak@gmail.com>
2016-08-31 16:44:24 +0200

    Add tvOS example project

--- Examples/RATreeViewTVExample/ViewController.swift
+++ Examples/RATreeViewTVExample/ViewController.swift
@@ -9,42 +9,57 @@
 import UIKit
 import RATreeView
 
-class ViewController: UITableViewController, RATreeViewDataSource {
-
+class ViewController: UIViewController, RATreeViewDataSource {
+    
+    var data : [DataObject] = DataObject.defaultTreeRootChildren()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let treeView = RATreeView()
         view.addSubview(treeView)
         treeView.frame = view.bounds
         treeView.dataSource = self
+        treeView.registerNib(UINib.init(nibName: String(TreeTableViewCell.self), bundle: nil), forCellReuseIdentifier: String(TreeTableViewCell.self))
     }
-
+    
     func treeView(treeView: RATreeView, numberOfChildrenOfItem item: AnyObject?) -> Int {
-        if let item = item {
-            return 0
+        if let item = item as? DataObject {
+            return item.children.count
         } else {
-            return 1
+            return data.count
         }
     }
-
+    
     func treeView(treeView: RATreeView, cellForItem item: AnyObject?) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWithIdentifier("cell") else {
-            fatalError()
+        guard let cell = treeView.dequeueReusableCellWithIdentifier(String(TreeTableViewCell.self)) as? TreeTableViewCell,
+            let item = item as? DataObject else {
+                fatalError()
         }
+        
+        let level = treeView.levelForCellForItem(item)
+        cell.setup(withTitle: item.name, detailsText: "Number of children: \(item.children.count)", level: level)
+        
         return cell
     }
-
+    
     func treeView(treeView: RATreeView, child index: Int, ofItem item: AnyObject?) -> AnyObject {
-        return "1"
+        if item == nil {
+            return data[index]
+        } else if let item = item as? DataObject {
+            return item.children[index]
+        } else {
+            fatalError()
+        }
     }
-
+    
     func treeView(treeView: RATreeView, canEditRowForItem item: AnyObject) -> Bool {
         return false
     }
-
+    
     func treeView(treeView: RATreeView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowForItem item: AnyObject) {
+        
     }
-
+    
 }
 
--- Examples/Shared/DataObject.swift
+++ Examples/Shared/DataObject.swift
@@ -31,3 +31,37 @@ class DataObject
         self.children = self.children.filter( {$0 !== child})
     }
 }
+
+
+extension DataObject {
+
+    static func defaultTreeRootChildren() -> [DataObject] {
+        let phone1 = DataObject(name: "Phone 1")
+        let phone2 = DataObject(name: "Phone 2")
+        let phone3 = DataObject(name: "Phone 3")
+        let phone4 = DataObject(name: "Phone 4")
+        let phones = DataObject(name: "Phones", children: [phone1, phone2, phone3, phone4])
+        
+        let notebook1 = DataObject(name: "Notebook 1")
+        let notebook2 = DataObject(name: "Notebook 2")
+        
+        let computer1 = DataObject(name: "Computer 1", children: [notebook1, notebook2])
+        let computer2 = DataObject(name: "Computer 2")
+        let computer3 = DataObject(name: "Computer 3")
+        let computers = DataObject(name: "Computers", children: [computer1, computer2, computer3])
+        
+        let cars = DataObject(name: "Cars")
+        let bikes = DataObject(name: "Bikes")
+        let houses = DataObject(name: "Houses")
+        let flats = DataObject(name: "Flats")
+        let motorbikes = DataObject(name: "Motorbikes")
+        let drinks = DataObject(name: "Drinks")
+        let food = DataObject(name: "Food")
+        let sweets = DataObject(name: "Sweets")
+        let watches = DataObject(name: "Watches")
+        let walls = DataObject(name: "Walls")
+        
+        return [phones, computers, cars, bikes, houses, flats, motorbikes, drinks, food, sweets, watches, walls]
+    }
+    
+}
